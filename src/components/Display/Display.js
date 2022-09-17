f5563b3945dc7df5306edda896eef8759a1232e2
Connor McCue <connor.mccue@comcast.net>
2022-09-16 17:16:26 -0700

    completed

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -3,9 +3,20 @@ import React from 'react';
 import './Display.css';
 
 export default function Display({ catchphraseArray }) {
+  const theRealArray = catchphraseArray.map((phrase) =>
+    <li key={phrase.toString()}>{phrase}</li>
+  );
+  
   return (
     <div className="display">
-      <h1>{catchphraseArray}</h1>
+      <ul>{theRealArray}</ul>
     </div>
   );
 }
+
+
+// {
+//   articles.map((article) => (
+//     <MyAmazingArticle caption={article.title} color={article.background} />
+//   ));
+// }
