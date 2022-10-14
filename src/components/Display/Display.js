21226d27beb79632e361ccb399547a0c30f1f311
Connor McCue <connor.mccue@comcast.net>
2022-10-13 18:59:50 -0700

    removed console statements

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -5,8 +5,6 @@ export default function Display({ catchphraseArray, menuItemsArray }) {
   const theRealArray = catchphraseArray.map((phrase, i) =>
     <h2 key={i}>Order Number {phrase} Items: {menuItemsArray[3 * i]}, {menuItemsArray[(3 * i) + 1]}, {menuItemsArray[(3 * i) + 2]}</h2>
   );
-  console.log(menuItemsArray);
-  console.log(theRealArray);
   return (
     <div className="display">
       <div>{theRealArray}</div>
