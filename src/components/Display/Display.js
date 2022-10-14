cb7f6a3f600fec1c1ae38c725f29e5cfdcb01caf
Connor McCue <connor.mccue@comcast.net>
2022-10-13 18:58:40 -0700

    fixed display error

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -3,9 +3,10 @@ import './Display.css';
 
 export default function Display({ catchphraseArray, menuItemsArray }) {
   const theRealArray = catchphraseArray.map((phrase, i) =>
-    <h2 key={i}>Order Number {phrase} Items: {menuItemsArray[i]}, {menuItemsArray[i + 1]}, {menuItemsArray[i + 2]}</h2>
+    <h2 key={i}>Order Number {phrase} Items: {menuItemsArray[3 * i]}, {menuItemsArray[(3 * i) + 1]}, {menuItemsArray[(3 * i) + 2]}</h2>
   );
-
+  console.log(menuItemsArray);
+  console.log(theRealArray);
   return (
     <div className="display">
       <div>{theRealArray}</div>
